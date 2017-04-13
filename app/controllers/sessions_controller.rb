a705c7fc703545f4f25cb28054f377dc1d531879
meryldakin <meryl.k.dakin@gmail.com>
2017-04-13 13:18:48 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to home_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to login_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'application#hello'
-  get '/login' => 'sessions#new'
+
+  root 'application#hello', as: :home
+  get '/login' => 'sessions#new', as: :login
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
